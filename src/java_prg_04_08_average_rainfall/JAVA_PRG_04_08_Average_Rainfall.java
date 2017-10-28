b0898d542f03f24343363b69716b3faa2dc53e3b
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-28 13:37:29 -0400

    Wrote the prompt, need to write the program.

---
+++ src/java_prg_04_08_average_rainfall/JAVA_PRG_04_08_Average_Rainfall.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_04_08_average_rainfall;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that uses nested loops to collect data and calculate the
+ * average rainfall over a period of years. First the program should ask for
+ * the total number of years. The outer loop will iterate once for each year.
+ * The inner loop will iterate 12 times, once for each month. After all
+ * iterations, the program should display the number of months, the total
+ * inches of rainfall, and the average rainfall per month for the entire
+ * period.
+ * 
+ * Input Validation: Do not accept a number less than 1 for the number of years.
+ * Do not accept negative numbers for the monthly rainfall.
+ */
+public class JAVA_PRG_04_08_Average_Rainfall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
